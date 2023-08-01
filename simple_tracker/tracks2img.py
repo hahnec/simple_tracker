93e428e0a62c7d23991917ebf8bf6b879653c37f
hahnec <christopher.hahne@unibe.ch>
2023-08-01 15:50:47 +0200

    feat(tracking): enable tracking capability

--- simple_tracker/tracks2img.py
+++ simple_tracker/tracks2img.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.ndimage import uniform_filter1d
 
+from .tracking2d import tracking2d
+
 
 def tracks2img(tracks, img_size, scale=1, mode=None):
 
@@ -32,20 +34,18 @@ def tracks2img(tracks, img_size, scale=1, mode=None):
         img[idcs[1], idcs[0]] = count
 
     elif mode == 'tracks':
-        
-         for i in range(len(tracks)):
-            
-            # get integer image coordinates
-            coords = np.round(tracks[i][:, :2]*scale).astype('int')
 
-            # remove out of grid bubbles (ie. the grid is too small)
-            valid = (0 < coords[:, 0]) & (coords[:, 0] < img_size[1]*scale) & (0 < coords[:, 1]) & (coords[:, 1] < img_size[0]*scale)
+        # init variables
+        min_len = 15
+        max_linking_distance = 2
+        max_gap_closing = 0
+        fps = 1000
 
-            # get valid indices and number of duplicates (counts)
-            idcs, count = np.unique(coords[valid].T, axis=1, return_counts=True)
-
-            # set pixels in image
-            img[idcs[1], idcs[0]] += count
+        # render based on Hungarian linker
+        #tracks = [f / wavelength for f in tracks]
+        tracks, tracks_interp = tracking2d(tracks, max_linking_distance=max_linking_distance, max_gap_closing=max_gap_closing, min_len=min_len, scale=1/fps, mode='interp')
+        #tracks = [np.hstack([p[:, :2] - origin[:2], p[:, 2:]]) for p in tracks]
+        img, vel = tracks2img(tracks, img_size=size, scale=10, mode='tracks')#velnorm')
 
     elif mode in ['vel_z', 'velnorm', 'velmean']:
 
