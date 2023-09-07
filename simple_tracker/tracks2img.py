f3fda954e6b9507be910f11cecd4657a25623a0d
hahnec <christopher.hahne@unibe.ch>
2023-09-07 14:55:36 +0200

    fix(fun): rectify argument in recursive function call

--- simple_tracker/tracks2img.py
+++ simple_tracker/tracks2img.py
@@ -62,7 +62,7 @@ def tracks2img(tracks, img_size, scale=1, mode=None, fps = 1000):
             tracks_result.extend(result)
 
         # recursive function call
-        img, vel = tracks2img(tracks_result, img_size=img_size, scale=scale, mode='all_in')
+        img, vel = tracks2img(tracks_result, img_size=img_size, scale=scale, mode=mode)
 
     elif mode in ['vel_z', 'velnorm', 'velmean']:
 
